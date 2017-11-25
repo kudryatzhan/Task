050dac12fea5d8538b0b079b0bb0bd0bc33b018e
Kudryatzhan Arziyev <karziyev@gmail.com>
2017-11-24 17:55:55 -0700

    Implemented Core Data Model

---
+++ Task/Core Data/Task+Convenience.swift
@@ -0,0 +1,21 @@
+//
+//  Task+Convenience.swift
+//  Task
+//
+//  Created by Kudryatzhan Arziyev on 11/24/17.
+//  Copyright © 2017 Kudryatzhan Arziyev. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Task {
+    
+    convenience init(name: String, notes: String? = nil, due: Date? = nil, context: NSManagedObjectContext = CoreDataStack.context) {
+        self.init(context: context)
+        
+        self.name = name
+        self.notes = notes
+        self.due = due
+    }
+}
