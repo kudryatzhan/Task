8895c33078c8462fd97ac3cf3d31c309cc80d234
Kudryatzhan Arziyev <karziyev@gmail.com>
2017-11-24 17:30:05 -0700

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Kudryatzhan Arziyev on 11/24/17.
+//  Copyright © 2017 Kudryatzhan Arziyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
